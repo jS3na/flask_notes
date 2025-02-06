f6241aac721fbab9f55c0bd49659d7fc25087671
js3na <passosjoaogabriel29@gmail.com>
2025-02-05 22:27:21 -0300

    fix: database url

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_NAME = os.getenv("DB_NAME")
 
 class ApplicationConfig:
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.getenv("SECRET_KEY")
