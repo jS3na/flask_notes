d331de10fb5c24d2c2d927142faac96c0435d808
js3na <passosjoaogabriel29@gmail.com>
2025-02-05 21:39:32 -0300

    create: requirements.txt

--- config.py
+++ config.py
@@ -20,4 +20,4 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url("redis://red-cui06n56l47c73f64teg:6379")
