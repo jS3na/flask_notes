cdb447f94e49de241109f6333388269a2008a03f
js3na <passosjoaogabriel29@gmail.com>
2025-02-06 15:29:39 -0300

    trying to fix session token

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from models import db
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
-CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://notes-green-sigma.vercel.app"])
+CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://notes-emdo.onrender.com"])
 
 db.init_app(app)
 migrate = Migrate(app, db)
